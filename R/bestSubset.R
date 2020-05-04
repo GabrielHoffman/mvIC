2c55784dcf20f564a50ed00b404880aebe2b55b1
GabrielHoffman <gabrielhoffman@mtscrma.msnyuhealth.org>
2020-05-04 19:37:28 -0400

    add other IC

--- R/bestSubset.R
+++ R/bestSubset.R
@@ -7,10 +7,10 @@
 #'
 #' @param exprObj matrix of expression data (g genes x n samples), or ExpressionSet, or EList returned by voom() from the limma package
 #' @param data data.frame with columns corresponding to formula
-#' @param variables array of variable names to be considered in the regression.  If variable should be considered as random effect, use '(1|A)'.
+#' @param variables array of variable names to be considered in the regression.  Random effects are not allowed
 #' @param maxk evaluate all combinations of variables up to size maxk
 #'
-#' @import data.table
+#' @importFrom dplyr as_tibble
 #' @importFrom utils combn
 #' @export
 bestSubsetSearch = function(Y, data, variables, maxk=5){
@@ -23,11 +23,19 @@ bestSubsetSearch = function(Y, data, variables, maxk=5){
     res = lapply(formArray, function(form){
       fit = lm(as.formula(form), data=data)
 
+      n = nrow(residuals(fit))
+      p = ncol(residuals(fit))
+
       res = lapply(c("EB", "none"), function(method){
-        res = lapply(c("AIC", "BIC", "sum AIC", "sum BIC"), function(criterion){
-          if( length(grep("sum", criterion)) > 0 & method != "none" ) return(NULL)
+        res = lapply(c("AIC", "BIC", "AICC", "CAIC", "sum AIC", "sum BIC"), function(criterion){
+         
+          # if( length(grep("sum", criterion)) > 0 & method != "none" ) return(NULL)
+          if( criterion %in% c("AICC", "CAIC") & (method != "none") ) return(NULL)
+          if( (method == "none") & !(criterion %in% c("sum AIC", "sum BIC")) & (p > n) ) return(NULL)     
+          if( (method == "EB") & (criterion %in% c("sum AIC", "sum BIC")) ) return(NULL)     
 
           score = mvIC(fit, shrink.method=method, criterion=criterion)
+
           data.frame(k=k,form=form, method=method, criterion=criterion, score = score[1], stringsAsFactors=FALSE)
         })
         do.call(rbind, res)
@@ -36,5 +44,7 @@ bestSubsetSearch = function(Y, data, variables, maxk=5){
     } )
     res = do.call(rbind, res)
   })
-  data.table(do.call(rbind, res))
+  as_tibble(do.call(rbind, res))
 }
+
+
--- R/evalCriterion.R
+++ R/evalCriterion.R
@@ -30,7 +30,7 @@
 #' @importFrom stats as.formula
 #' @importFrom dplyr as_tibble
 #' @export
-mvForwardStepwise = function( exprObj, baseFormula, data, variables, criterion = c("AIC", "BIC", "sum AIC", "sum BIC"), shrink.method = c("var_equal", "var_unequal", "EB", "none"), nparamsMethod = c("edf", "countLevels", "lme4"), deltaCutoff = 5, verbose=TRUE,...  ){
+mvForwardStepwise = function( exprObj, baseFormula, data, variables, criterion = c("AIC", "BIC", "AICC", "CAIC", "sum AIC", "sum BIC"), shrink.method = c("var_equal", "var_unequal", "EB", "none"), nparamsMethod = c("edf", "countLevels", "lme4"), deltaCutoff = 5, verbose=TRUE,...  ){
 
 	criterion = match.arg(criterion)
 	shrink.method  = match.arg(shrink.method)
@@ -191,7 +191,7 @@ mvForwardStepwise = function( exprObj, baseFormula, data, variables, criterion =
 #' @import variancePartition 
 #'
 #' @export
-mvIC_fit = function( exprObj, formula, data, criterion = c("AIC", "BIC", "sum AIC", "sum BIC"), shrink.method = c("var_equal", "var_unequal", "EB", "none"), nparamsMethod = c("edf", "countLevels", "lme4"), verbose=FALSE, ... ){
+mvIC_fit = function( exprObj, formula, data, criterion = c("AIC", "BIC", "AICC", "CAIC", "sum AIC", "sum BIC"), shrink.method = c("var_equal", "var_unequal", "EB", "none"), nparamsMethod = c("edf", "countLevels", "lme4"), verbose=FALSE, ... ){
 
 	criterion = match.arg(criterion)
 	shrink.method  = match.arg(shrink.method)
@@ -382,7 +382,7 @@ nparam = function( object, nparamsMethod = c("edf", "countLevels", "lme4")){
 #' 
 #' @importFrom methods is
 #' @export
-mvIC = function( fitList, criterion =c("AIC", "BIC", "sum AIC", "sum BIC"), shrink.method = c("var_equal", "var_unequal", "EB", "none"), nparamsMethod = c("edf", "countLevels", "lme4"), ...){
+mvIC = function( fitList, criterion =c("AIC", "BIC", "AICC", "CAIC", "sum AIC", "sum BIC"), shrink.method = c("var_equal", "var_unequal", "EB", "none"), nparamsMethod = c("edf", "countLevels", "lme4"), ...){
 
 	criterion = match.arg(criterion)
 	shrink.method  = match.arg(shrink.method)
@@ -410,7 +410,7 @@ mvIC = function( fitList, criterion =c("AIC", "BIC", "sum AIC", "sum BIC"), shri
 #' @param ... other arguments passed to logDet
 #' 
 #' @importFrom methods new
-mvIC_from_residuals = function( residMatrix, m, criterion =c("AIC", "BIC", "sum AIC", "sum BIC"), shrink.method = c("var_equal", "var_unequal", "EB", "none"), ... ){
+mvIC_from_residuals = function( residMatrix, m, criterion =c("AIC", "BIC", "AICC", "CAIC", "sum AIC", "sum BIC"), shrink.method = c("var_equal", "var_unequal", "EB", "none"), ... ){
 
 	criterion = match.arg(criterion)
 	shrink.method  = match.arg(shrink.method)
@@ -418,7 +418,12 @@ mvIC_from_residuals = function( residMatrix, m, criterion =c("AIC", "BIC", "sum 
 	n = ncol(residMatrix) # number of samples
 	p = nrow(residMatrix) # number of response variables
 
-	if( criterion %in% c("AIC", "BIC") ){
+	if( criterion %in% c("AIC", "BIC", "AICC", "CAIC") ){
+
+		if( criterion %in% c("AICC", "CAIC") & n < p){
+			stop(paste("Criterion", criterion, "cannot be evaluated when n < p"))
+		}
+		
 		# compute log determinant explicitly
 		# slower and not defined for low rank matrices
 		# dataTerm = n * determinant(crossprod(residMatrix), log=TRUE)$modulus[1]
@@ -448,7 +453,9 @@ mvIC_from_residuals = function( residMatrix, m, criterion =c("AIC", "BIC", "sum 
 		# doi:10.1214/15-EJS1022
 		penalty = switch( criterion, 
 						"AIC" 	= 2 * (p*(m-1) + gdf_cov),
-						"BIC" 	= log(n) * (p*(m-1) + gdf_cov))
+						"BIC" 	= log(n) * (p*(m-1) + gdf_cov),
+						"AICC"	= 2 * n*(p*(m-1) + gdf_cov) / (n-(m-1) - p - 1),
+						"CAIC"	= (1+log(n)) * (p*(m-1) + gdf_cov))
 
 		# retrun data term plus penalty
 		res = dataTerm + penalty
